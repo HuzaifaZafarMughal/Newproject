68c455f25226d209cab8c6f8061205eeec74a878
Palwasha <palwasha7223@gmail.com>
2020-11-08 02:29:31 +0500

    V 1.0

---
+++ app/src/main/java/com/example/myapplication/Frame_layout.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Frame_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_layout);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Scroll_View.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scroll_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scroll__view);
+    }
+}
